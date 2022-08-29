a4922f4d279b93485f6f88398f21815374b6923a
Popova Ekaterina <popova_katerina97@mail.ru>
2022-08-29 13:32:07 +0500

    Task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите числа (через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); // Ввод массива с клавиатуры
+
+int count = 0;
+for (int i =0; i<array.Length; i++ )
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Количество чисел в массиве больше 0 = "+ count);
+
+
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
